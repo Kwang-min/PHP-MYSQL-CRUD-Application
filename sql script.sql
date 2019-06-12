dab8516520708d396b5afd32a8348ecbb9f20848
Kwang-min <48762168+Kwang-min@users.noreply.github.com>
2019-06-13 03:00:01 +0900

    create schema, table

---
+++ sql script.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE opentutorials;
+CREATE TABLE topic(
+id int(11) NOT NULL AUTO_INCREMENT,
+title varchar(45) NOT NULL,
+description text,
+created datetime NOT NULL,
+PRIMARY KEY(id)
+);
