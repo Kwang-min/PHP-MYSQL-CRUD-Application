d791c8e73c9b2391850d7fe9357adfd483dc9fe1
Kwang-min <48762168+Kwang-min@users.noreply.github.com>
2019-06-17 05:05:51 +0900

    security(filtering & escaping)

--- process_create.php
+++ process_create.php
@@ -1,7 +1,12 @@
 <?php
   $conn = mysqli_connect('localhost','root','admin1234','opentutorials');
+
+  $filtered = array('title'=>mysqli_real_escape_string($conn,$_POST['title']),
+                    'description'=>mysqli_real_escape_string($conn,$_POST['description']));
+
+
   $sql = "INSERT INTO topic (title, description, created)
-          VALUES('{$_POST['title']}', '{$_POST['description']}', NOW())";
+          VALUES('{$filtered['title']}', '{$filtered['description']}', NOW())";
   $result = mysqli_query($conn, $sql);
   if($result === false){
     echo "데이터 처리 도중에 문제가 생겼습니다 관리자에게 문의하세요";
